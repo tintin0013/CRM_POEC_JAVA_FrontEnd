4294b0b8ab7784868d4c9f1a9c94857c00e4f83a
Tintin0013 <arnaudpinatel@arnauds-mbp.home>
2023-12-20 15:50:49 +0100

    essai affichage channels

--- src/app/Components/channels/channels.component.ts
+++ src/app/Components/channels/channels.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ChannelService } from '../../Services/channel.service';
+import { CommonModule } from '@angular/common';
+import { FetcherService } from '../../Services/fetcher.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-channels',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './channels.component.html',
   styleUrl: './channels.component.css'
 })
 export class ChannelsComponent {
+
+  constructor(public channel: ChannelService){}
 
 }
