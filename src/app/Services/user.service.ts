46ba08d774edd85e3161eccc72fb19d894311fbe
Tintin0013 <arnaudpinatel@arnauds-mbp.home>
2023-12-20 15:18:52 +0100

    mise a jour du user.service

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { User } from '../Models/User';
+import { UserStatus } from '../Models/UserStatus';
+import { HttpClient } from '@angular/common/http';
+import { FetcherService } from './fetcher.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: FetcherService) { }
+
+  
+
+getUserByIdService(id: number){
+  return this.http.getUserById(id);
+}
+
+postUserService(user: User) {
+  return this.http.postUser(user);
+}
+
+updateUserService(user: User) {
+  return this.http.updateUser(user);
+}
+
+deleteUserService(id: number) {
+  return this.http.deleteUser(id);
+}
 }
