d239ec6127287bbedede121ee04357b82c60c635
Tintin0013 <arnaudpinatel@arnauds-mbp.home>
2023-12-20 16:42:06 +0100

    ajout affichage message

--- src/app/Components/messages/messages.component.ts
+++ src/app/Components/messages/messages.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../../Services/message.service';
+import { CommonModule } from '@angular/common';
+import { FetcherService } from '../../Services/fetcher.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent {
+
+  constructor(public message: MessageService) {}
 
 }
